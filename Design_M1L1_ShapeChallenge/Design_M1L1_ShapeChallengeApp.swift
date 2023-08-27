96f98af2a8f614a54a5c58f605a5a6cf0f9c2bfb
Tom Montgomery <montgomery3681@gmail.com>
2023-08-27 17:45:23 -0600

    Initial Commit

---
+++ Design_M1L1_ShapeChallenge/Design_M1L1_ShapeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L1_ShapeChallengeApp.swift
+//  Design_M1L1_ShapeChallenge
+//
+//  Created by tom montgomery on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L1_ShapeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
